585362f29138801686cf07b610a028e249ae8da4
Anjali3259 <kumarianjali110103@gmail.com>
2024-09-14 13:06:11 +0530

    Add files via upload

---
+++ About.js
@@ -0,0 +1,20 @@
+// script.js
+document.getElementById("dashboard-btn").addEventListener("click", function () {
+    window.location.href = "dashboard.html";
+});
+
+document.getElementById("course-btn").addEventListener("click", function () {
+    window.location.href = "course_creation.html";
+});
+
+document.getElementById("student-progress-btn").addEventListener("click", function () {
+    window.location.href = "progress.html";
+});
+
+document.getElementById("teacher-tracker-btn").addEventListener("click", function () {
+    window.location.href = "Teacher_tracker.html";
+});
+
+document.getElementById("quizzes-btn").addEventListener("click", function () {
+    window.location.href = "quiz.html";
+});
